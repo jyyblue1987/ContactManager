f23c7705b578adc43f68bbbc953f6111c57972fd
jyyblue <jyyblue1987@outlook.com>
2021-03-30 03:18:45 +0800

    [add] add detail page.

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -9,5 +11,11 @@ public class PageController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam(name="id", required=false, defaultValue="10") int id, Model model) {
+        model.addAttribute("id", id);
+        return "detail";
     }
 }
