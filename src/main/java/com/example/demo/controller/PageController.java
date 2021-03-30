fc239dae42204c7af89e62e1c63e73d6b355ffdd
jyyblue <jyyblue1987@outlook.com>
2021-03-30 10:04:11 +0800

    [add] add some comments

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Contact;
 import com.example.demo.service.IContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,33 +7,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 
 @Controller
 public class PageController {
     @Autowired
     IContactService contactService;
 
+    // Contact List Page
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "index"; // load index.html
     }
 
+    // Contact Detail Page
     @GetMapping("/detail")
     public String detail(@RequestParam(name="id", required=false, defaultValue="10") Long id, Model model) {
-//        var contacts = (List<Contact>) contactService.findById(id);
-//        model.addAttribute("id", contacts.get(0).getFull_name());
-        var val = contactService.findById(id);
-        if( val.isPresent() ) {
-            Contact contact = (Contact) val.get();
-            model.addAttribute("model", contact);
-        }
-        else
-        {
-            model.addAttribute("email", 0);
-        }
-
-        return "detail";
+        return "detail"; // load detail.html
     }
 }
--- src/main/java/com/example/demo/controller/RestApiController.java
+++ src/main/java/com/example/demo/controller/RestApiController.java
@@ -3,13 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.model.Contact;
 import com.example.demo.service.IContactService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// Rest Api Handler
 @RestController
 public class RestApiController {
     @Autowired
